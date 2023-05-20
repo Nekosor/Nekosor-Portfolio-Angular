a81716314dc9395bddd2b18131a4a29a5cced3c1
Nekosor <felipereyesingunza@gmail.com>
2023-05-19 23:40:46 -0500

    Actualización
    Update!!

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  sidebarOpen = false;
+  showHamburgerIcon = false;
 
+  ngOnInit(): void {
+    this.checkScreenWidth();
+  }
+
+  toggleSidebar(): void {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: any): void {
+    this.checkScreenWidth();
+
+
+  }
+  private checkScreenWidth(): void {
+    this.showHamburgerIcon = window.innerWidth < 768;
+    if (!this.showHamburgerIcon) {
+      this.sidebarOpen = true;
+    }
+  }
 }
